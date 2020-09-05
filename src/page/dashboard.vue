78221889d6aa28629d4c408534c9c92d38769de4
hejiyun <2879722585@qq.com>
2020-09-05 10:38:58 +0800

    详情展示组件封装完成

--- src/page/dashboard.vue
+++ src/page/dashboard.vue
@@ -1,27 +1,159 @@
 <template>
   <div class="dashboard-style">
     <el-row>欢迎您 {{ sysUserName }}</el-row>
-    <SearchForm :options="row">
-      <template slot="password2233">
-        <span>这里是新增的内容</span>
-      </template>
-      <template slot="btn">
-        <el-button>点击我</el-button>
-      </template>
-    </SearchForm>
+    <SearchForm :options="row"/>
+    <detail-view :options="detailOptions" :data="baseText" />
     <el-row class="footer-div">copyright @ 昆山宝唯信息科技有限公司上海分公司</el-row>
   </div>
 </template>
 <script>
 import SearchForm from '@component/Form'
+import DetailView from '@component/common/DetailView'
 import { InputC, SelectC, CheckBox, DateC, NumberInput, Radio, Switch, TextArea, Upload, DownLoad } from '@component/Form/ExportItem.js'
 export default {
   components: {
-    SearchForm
+    SearchForm,
+    DetailView
   },
   data() {
     return {
       sysUserName: '',
+      detailOptions: [{
+        label: '发货单号',
+        key: 'billNo' // data中对应的字段
+      }, {
+        label: '订单号',
+        key: 'orderNo',
+        type: 'link',
+        to: '/TradingPlatform/List/ListDetail'
+      }, {
+        label: '来源单号',
+        key: 'sourceBillNo'
+      },
+      {
+        label: '网店交易单号',
+        key: 'shopBillNo'
+      }, {
+        label: '发货单状态',
+        key: 'billStatusName'
+      }, {
+        label: '发货单生成时间',
+        key: 'createTime'
+      }, {
+        label: '发货单下发时间',
+        key: 'billDate'
+      }, {
+        label: '发货时间',
+        key: 'deliveryTime'
+      }, {
+        label: '业务类型',
+        key: 'bizTypeName'
+      }, {
+        label: '客商编码',
+        key: 'vendCustCode'
+      },
+      {
+        label: '客商名称',
+        key: 'vendCustName'
+      }, {
+        label: '调拨类型',
+        key: 'allocateType'
+      }, {
+        label: '调拨方式名称',
+        key: 'allocateModeName'
+      }, {
+        label: '调拨优先级',
+        key: 'allocatePriority'
+      }, { // 增加详情信息展示
+        label: '发运方式',
+        key: 'transportMethodName'
+      }, {
+        label: '所属批次',
+        key: 'batchMark'
+      }, {
+        label: '预计到货时间',
+        key: 'expectArrivalTime'
+      }, {
+        label: '其他单号',
+        key: 'otherBillNo' // 修改结束
+      }, {
+        label: '发货仓代码',
+        key: 'fromExternalWarehouseCode'
+      }, {
+        label: '发货仓名称',
+        key: 'fromExternalWarehouseName'
+      }, {
+        label: '目的仓代码',
+        key: 'toExternalWarehouseCode'
+      }, {
+        label: '目的仓名称',
+        key: 'toExternalWarehouseName'
+      },
+      {
+        label: '收件人省市区',
+        key: 'provinceCityArea'
+      }, {
+        label: '收件人地址',
+        key: 'address'
+      }, {
+        label: '收件人姓名',
+        key: 'contact'
+      }, {
+        label: '收件人电话',
+        key: 'phone'
+      }, {
+        label: '是否打印发票',
+        key: 'PrintInvoiceFlag',
+        type: 'radio'
+      }, {
+        label: '是否货票同行',
+        key: 'WithInvoiceFlag',
+        type: 'radio'
+      }, {
+        label: '备注',
+        key: 'remark'
+      }],
+      baseText: {
+        billNo: '1991224198210519096-D001',
+        orderNo: 'SO3951991224198210519096',
+        orderSource: '',
+        bwOrderNo: 'SO3951991224198210519096',
+        sourceBillNo: '',
+        shopBillNo: '2057',
+        billStatus: '20',
+        billStatusName: '待下发',
+        createTime: '2020-09-03 17:43:40',
+        billDate: '2020-09-03 17:43:40',
+        deliveryTime: null,
+        bizType: 'JITX_DISPATCH',
+        bizTypeName: 'JITX发货',
+        vendCustCode: '47001257',
+        vendCustName: '唯品会',
+        allocateModeCode: '',
+        allocateModeName: '',
+        allocatePriority: '',
+        allocateType: '',
+        fromExternalWarehouseCode: 'WH190JIT',
+        fromExternalWarehouseName: 'JIT测试仓HK',
+        toExternalWarehouseCode: '',
+        toExternalWarehouseName: '',
+        provinceCityArea: '福建省 宁德市 古田县',
+        address: '鹤塘镇坊下烟酒行',
+        contact: '李*婷',
+        phone: '01000009999',
+        printInvoiceFlag: 0,
+        withInvoiceFlag: 0,
+        remark: '',
+        totalBoxNo: null,
+        totalSkuCount: 0,
+        totalExpectQty: 2,
+        totalActualQty: null,
+        transportMethodCode: '',
+        transportMethodName: '',
+        expectArrivalTime: null,
+        otherBillNo: '',
+        batchNo: '',
+        batchMark: '' }, // 详情数据来源变量
       row: [
         {
           label: '输入框',
@@ -56,11 +188,6 @@ export default {
           label: '输入框2',
           prop: 'password2',
           cmp: NumberInput
-        },
-        {
-          label: '输入框6',
-          prop: 'password2233',
-          slot: true
         },
         {
           label: '日期选择',
